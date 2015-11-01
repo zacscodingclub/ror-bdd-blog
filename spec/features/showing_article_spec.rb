89492c8510576ffef3a70c0df6cb605be05301c4
Zachary Baston <zbaston@gmail.com>
2015-11-01 16:26:00 -0600

    implement show Article

---
+++ spec/features/showing_article_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "Showing Article" do
+  before do
+    @article = Article.create(title: "Test Title", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+  end
+
+  scenario "Show the article" do
+    visit "/"
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+  end
+end
