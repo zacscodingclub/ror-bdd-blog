b4c5e59b72e624a3afe581e4b6136a72b5032667
Zachary Baston <zbaston@gmail.com>
2015-11-02 08:59:55 -0600

    add feature Edit Article

---
+++ spec/features/editing_article_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.feature "Creating Articles" do
+  before do
+    @article = Article.create(title: "Test Title", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+  end
+
+  scenario "A user updates an article" do
+    visit "/"
+    click_link @article.title
+    click_link "Edit Article"
+
+    fill_in "Title", with: "Updated Article"
+    fill_in "Body", with: "Updated Lorem Body"
+    click_button "Update Article"
+
+    expect(page).to have_content("Article has been updated")
+    expect(page.current_path).to eq(article_path(@article))
+  end
+
+  scenario "A user fails to update an article" do
+    visit "/"
+    click_link @article.title
+    click_link "Edit Article"
+
+    fill_in "Title", with: ""
+    fill_in "Body", with: ""
+    click_button "Update Article"
+
+    expect(page).to have_content("Article has not been updated")
+    expect(page.current_path).to eq(article_path(@article))
+  end
+end
