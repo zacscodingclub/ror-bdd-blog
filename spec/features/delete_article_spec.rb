094c461fc0ee57fe6db50b23d05f830bd0cd3c4d
Zachary Baston <zbaston@gmail.com>
2015-11-04 10:19:05 -0600

    custom css for author on index

--- spec/features/delete_article_spec.rb
+++ spec/features/delete_article_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.feature "Deleting an Article"  do
   before do
     @john = User.create(email: "john@example.com", password: "password")
-    login_as(@john)
     @article = Article.create(title: "Test Title", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user: @john)
+
+    login_as(@john)
   end
 
   scenario "A user deletes an article" do
